89a8fcf9e2ff129ee8cd7388cd03da7a202a37bf
btweedster <morgab04@email.franklin.edu>
2026-09-16 20:28:06 -0400

    DataConnect and TestDataConnect added

--- src/DataConnect.java
+++ src/DataConnect.java
@@ -9,10 +9,10 @@ import java.util.HashMap;
 public class DataConnect {
     Map<Character,Map<String,Media>> contents;
     Map<String,Patron> patrons;
-    // Integer bookCount = 0;
-    // Integer musicCount = 0;
-    // Integer filmCount = 0;
-    // Integer patronCount = 0;
+    Integer bookCount = 0;
+    Integer musicCount = 0;
+    Integer filmCount = 0;
+    Integer patronCount = 0;
 
     public DataConnect() {
         patrons = new HashMap<String,Patron>();
@@ -23,19 +23,55 @@ public class DataConnect {
     }
 
     public void addMedia(Media m) {
-        Map<String,Media> mediaMap = contents.get(m.getId().charAt(0));
-        String newID = m.getId() + String.format("%07d",mediaMap.size() + 1);
+        char charKey = m.getId().charAt(0);
+        Integer count;
+
+        if (charKey == 'B') {
+            count = bookCount;
+        } else if (charKey == 'M') {
+            count = musicCount;
+        } else {
+            count = filmCount;
+        }
+
+        Map<String,Media> mediaMap = contents.get(charKey);
+        String newID = charKey + String.format("%07d",count + 1);
         m.setId(newID);
         mediaMap.put(m.getId(),m);
+        
+        if (charKey == 'B') {
+            bookCount++;
+        } else if (charKey == 'M') {
+            musicCount++;
+        } else {
+            filmCount++;
+        }
+    }
+
+    public boolean removeMedia(String id) {
+        char charKey = id.charAt(0);
+        Map<String,Media> mediaMap = contents.get(charKey);
+        if (mediaMap.containsKey(id)) {
+            mediaMap.remove(id);
+            return true;
+        }
+        return false;
     }
 
     public Media getMedia(String id) {
         return contents.get(id.charAt(0)).get(id);
-    } 
+    }
 
-    public boolean addPatron(Patron p) {
-        if (!patrons.containsKey(p.getId())) {
-            patrons.put(p.getId(),p);
+    public void addPatron(Patron p) {
+        String newID = "P" + String.format("%07d",patronCount + 1);
+        p.setId(newID);
+        patrons.put(p.getId(),p);
+        patronCount += 1;
+    }
+
+    public boolean removePatron(String id) {
+        if (patrons.containsKey(id)) {
+            patrons.remove(id);
             return true;
         }
         return false;
---
+++ test/TestDataConnect.java
@@ -0,0 +1,87 @@
+package test;
+
+import src.Media;
+import src.Book;
+import src.DataConnect;
+import src.Music;
+import src.Film;
+import src.Patron;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestDataConnect {
+    
+    @Test 
+    public void testConstr() {
+        DataConnect dc = new DataConnect();
+        assertNotNull(dc);
+    }
+
+    @Test 
+    public void testAddRemovePatron() {
+        DataConnect dc = new DataConnect();
+        Patron p = new Patron("Bilbo Baggins");
+        dc.addPatron(p);
+        String testPatronID = "P0000001";
+        assertEquals(p.getId(),testPatronID);
+        assertEquals(dc.getPatron(testPatronID).getName(),"Bilbo Baggins");
+
+        Patron p2 = new Patron("Frodo Baggins");
+        dc.addPatron(p2);
+        assertEquals(dc.getPatron("P0000002").getName(),"Frodo Baggins");
+        assertTrue(dc.removePatron(p2.getId()));
+
+        Patron p3 = new Patron("Samwise Gamgee");
+        dc.addPatron(p3);
+        assertEquals(dc.getPatron("P0000003").getName(),"Samwise Gamgee");
+    }
+
+    @Test 
+    public void testAddMedia() {
+        Media b1 = new Book("The Hobbit","J.R.R. Tolkien");
+        Media b2 = new Book("The Lord of the Rings","J.R.R. Tolkien");
+        Media b3 = new Book("The Silmarillion","J.R.R. Tolkien");
+        Media m1 = new Music("A Hard Day's Night","The Beatles");
+        Media m2 = new Music("Revolver","The Beatles");
+        Media m3 = new Music("Let It Be","The Beatles");
+        Media f1 = new Film("Star Wars",1977);
+        Media f2 = new Film("The Empire Strikes Back",1980);
+        Media f3 = new Film("Return of the Jedi",1983);
+
+        DataConnect dc = new DataConnect();
+
+        dc.addMedia(b1);
+        assertEquals(b1.getId(),"B0000001");
+        assertEquals(dc.getMedia("B0000001").getTitle(),"The Hobbit");
+        dc.addMedia(b2);
+        assertEquals(b2.getId(),"B0000002");
+        assertEquals(dc.getMedia("B0000002").getTitle(),"The Lord of the Rings");
+        assertTrue(dc.removeMedia(b2.getId()));
+        dc.addMedia(b3);
+        assertEquals(b3.getId(),"B0000003");
+        assertEquals(dc.getMedia("B0000003").getTitle(),"The Silmarillion");
+
+        dc.addMedia(m1);
+        assertEquals(m1.getId(),"M0000001");
+        assertEquals(dc.getMedia("M0000001").getTitle(),"A Hard Day's Night");
+        dc.addMedia(m2);
+        assertEquals(m2.getId(),"M0000002");
+        assertEquals(dc.getMedia("M0000002").getTitle(),"Revolver");
+        assertTrue(dc.removeMedia(m2.getId()));
+        dc.addMedia(m3);
+        assertEquals(m3.getId(),"M0000003");
+        assertEquals(dc.getMedia("M0000003").getTitle(),"Let It Be");
+
+        dc.addMedia(f1);
+        assertEquals(f1.getId(),"F0000001");
+        assertEquals(dc.getMedia("F0000001").getTitle(),"Star Wars");
+        dc.addMedia(f2);
+        assertEquals(f2.getId(),"F0000002");
+        assertEquals(dc.getMedia("F0000002").getTitle(),"The Empire Strikes Back");
+        assertTrue(dc.removeMedia(f2.getId()));
+        dc.addMedia(f3);
+        assertEquals(f3.getId(),"F0000003");
+        assertEquals(dc.getMedia("F0000003").getTitle(),"Return of the Jedi");
+    }
+}
